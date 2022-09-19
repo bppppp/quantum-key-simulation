98ca5f142d220f84f9b9f2266ab31626982806bc
bppppp <1091552981@qq.com>
2022-09-19 17:05:11 +0800

    feat: 完成攻击部分的布局

--- src/page/QuantumKeyDistribution/page/QuantumToClass.jsx
+++ src/page/QuantumKeyDistribution/page/QuantumToClass.jsx
@@ -9,7 +9,7 @@ const QuantumToClass = () => {
   const [currentStatus, setCurrentStatus] = useState('process') //指定当前步骤的状态，可选 wait process finish error
   
   const stepsList = [['第 1 步','第一步'],['第 2 步','第一步'],['第 3 步','第一步'],['第 4 步','第一步'],['第 5 步','第一步'],['第 6 步','第一步'],['第 7 步','第一步'],['第 8 步','第一步'],['第 9 步','第一步']]
-  
+
   const nextSucceseStep = () => {
     if (currentStep < 0) {
       return 
@@ -33,7 +33,7 @@ const QuantumToClass = () => {
           {stepsList.map((value) => <Step title={value[0]} description={value[1]} key={value[0]} />)}
         </Steps>
       </div>
-      <div className="quantum-to-class-operation-up">
+      <div className="quantum-to-class-content-up">
         <div className="user-container">
           <div className="user-container-left"><UserOutlined style={{ fontSize: '150px', color: '#08c' }} />
             <div>Alice</div>
@@ -56,13 +56,31 @@ const QuantumToClass = () => {
           <div>123</div>
         </div>
       </div>
-    </div>
-    <div className="quantum-to-class-operation">
-      <div className="quantum-to-class-operation-down">
+      <div className="quantum-to-class-content-down">
         <div></div>
         <Button onClick={nextSucceseStep} disabled={currentStatus === 'error'} size='large'>下一步</Button>
         <Button onClick={restart} size='large'>重新开始</Button>
         {/* <Button onClick={errorHappen}>发生错误</Button> */}
+      </div>
+    </div>
+    <div className="quantum-to-class-operation">
+      <div className='unilateral-attack'>
+        <div className='attact-title'>单边攻击</div>
+        <div className='attact-type'>
+          <Button danger disabled={false}>拦截攻击</Button>
+          <Button danger>测量重发攻击</Button>
+          <Button danger>纠缠测量攻击</Button>
+          <Button danger>篡改攻击</Button>
+        </div>
+      </div>
+      <div className='bilateral-attack'>
+      <div className='attact-title'>双边攻击</div>
+        <div className='attact-type'>
+          <Button danger disabled={false}>拦截攻击</Button>
+          <Button danger>测量重发攻击</Button>
+          <Button danger>纠缠测量攻击</Button>
+          <Button danger>篡改攻击</Button>
+        </div>
       </div>
     </div>
   </div>
